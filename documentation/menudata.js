f860f7522639534e5ca383c899fb3b7c7ec0a5f7
Mubeg <35347962+Mubeg@users.noreply.github.com>
2019-08-30 11:45:50 +0300

    Add files via upload

---
+++ documentation/menudata.js
@@ -0,0 +1,7 @@
+var menudata={children:[
+{text:"Титульная страница",url:"index.html"},
+{text:"Файлы",url:"files.html",children:[
+{text:"Файлы",url:"files.html"},
+{text:"Список членов всех файлов",url:"globals.html",children:[
+{text:"Указатель",url:"globals.html"},
+{text:"Функции",url:"globals_func.html"}]}]}]}
